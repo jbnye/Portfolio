33ee86ecf14b59d010eb6f46f78db5fe7d75bddf
Jacob Nye <jn3268@gmail.com>
2025-09-04 23:30:42 -0400

    working on homepage

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,12 +2,14 @@
 
 export default function HomePage() {
     return (
-        <div className="flex-col justify-center h-full w-full">
-            <h1>Home Page</h1>
-            <div>
-                <img src="/vite.svg">
-                </img>
+        <div className="w-[50%] justify-center items-center flex-col">
+            <div className="bg-red-500 border-b-black border-2 h-[200px] w-[200px] rounded-full flex items-center justify-center overflow-hidden">
+                <img src="/vite.svg" alt="profile" className="h-full w-full object-cover" />
             </div>
+            <h1>Jacob Nye</h1>
+            <p>
+                Hello, I'm an aspiring software engineer that loves making websites and solving problems involing my hobbies of compeittive gaming and films. Feel free to check out some examples in projects. 
+            </p>
         </div>
     )
 }
