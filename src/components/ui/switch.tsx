ffcf94e90520ce5c9a482f5e9a61f0649e627694
Jacob Nye <jn3268@gmail.com>
2025-09-24 12:09:24 -0400

    made an animation for the dark mode sldier and tweaks

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import * as SwitchPrimitive from "@radix-ui/react-switch"
-
 import { cn } from "@/lib/utils"
 
 function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimitive.Root>) {
@@ -8,20 +7,25 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
     <SwitchPrimitive.Root
       {...props}
       className={cn(
-        "peer inline-flex h-[1.15rem] w-16 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50",
-        "bg-white data-[state=checked]:bg-[#1e2228] cursor-pointer",
+        "relative inline-flex h-[1.3rem] w-16 shrink-0 items-center rounded-full cursor-pointer",
+        "bg-white dark:bg-test-black2",
+        "border border-black dark:border-white",
+        "transition-colors outline-none shadow-xs",
+        "focus-visible:ring-[3px] focus-visible:ring-ring/50",
+        "disabled:cursor-not-allowed disabled:opacity-50",
+        className
       )}
     >
       <SwitchPrimitive.Thumb
         className={cn(
-          "block rounded-full ring-0 transition-transform overflow-hidden  ",
-          "w-[1.15rem] h-[1.15rem]",
-          "data-[state=unchecked]:translate-x-0",
-          "data-[state=checked]:translate-x-[calc(4rem-1.15rem)]", 
-          "bg-black data-[state=checked]:bg-black"
+          "absolute block rounded-full overflow-hidden",
+          "w-[1.3rem] h-[1.3rem]",
+          "bg-black data-[state=checked]:bg-black",
+          "transition-transform duration-500 ease-in-out",
+          "data-[state=checked]:translate-x-[calc(4rem-1.15rem-0.25rem)] data-[state=checked]:rotate-180"
         )}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-40 -40 80 80" className="w-full h-full rotate-340" >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-40 -40 80 80" className="w-full h-full">
           <circle r="39" />
           <path fill="#fff" d="M0,38a38,38 0 0 1 0,-76a19,19 0 0 1 0,38a19,19 0 0 0 0,38" />
           <circle r="5" cy="19" fill="#fff" />
@@ -29,7 +33,7 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
         </svg>
       </SwitchPrimitive.Thumb>
     </SwitchPrimitive.Root>
-  );
+  )
 }
 
 export { Switch }
